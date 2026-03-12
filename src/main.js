9ee20711d1049ca961fb408111690fa5c1775352
Adit-009 <147326770+Adit-009@users.noreply.github.com>
2026-03-12 15:19:50 +0530

    fix views path

--- src/main.js
+++ src/main.js
@@ -2,32 +2,52 @@ const express = require("express");
 const app = express();
 const Model = require("../Model/User");
 const path = require("path");
+
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json())
+app.use(express.json());
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "../views"));
+
 app.get("/", (req, res) => {
   res.render("index");
 });
+
 app.post("/submit", async (req, res) => {
-  const { name, email, age } = req.body;
-  await Model.create({
-    name,
-    email,
-    age,
-  });
-  res.redirect("/view");
+  try {
+    const { name, email, age } = req.body;
+
+    await Model.create({
+      name,
+      email,
+      age,
+    });
+
+    res.redirect("/view");
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error saving user");
+  }
 });
 
-app.get('/view',async (req,res)=>{
-    const allUsers = await Model.find() 
-    res.render("view",{users: allUsers})
-})
+app.get("/view", async (req, res) => {
+  try {
+    const allUsers = await Model.find();
+    res.render("view", { users: allUsers });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error loading users");
+  }
+});
 
-app.post('/delete/:id',async(req,res)=>{
-    console.log(req.params.id)
-    const user = await Model.findByIdAndDelete(req.params.id)
-    res.redirect('/view')
-})
+app.post("/delete/:id", async (req, res) => {
+  try {
+    await Model.findByIdAndDelete(req.params.id);
+    res.redirect("/view");
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error deleting user");
+  }
+});
 
-module.exports = app
+module.exports = app;
