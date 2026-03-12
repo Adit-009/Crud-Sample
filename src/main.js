301d33dd46cb50a4d61b490f1e1193d49750cf96
Adit-009 <147326770+Adit-009@users.noreply.github.com>
2026-03-12 16:09:50 +0530

    fix views path

--- src/main.js
+++ src/main.js
@@ -31,14 +31,14 @@ app.post("/submit", async (req, res) => {
 });
 
 app.get("/test", (req,res)=>{
-  res.send("Server OK");
+  res.render("view");
 });
 
 
-app.get("/view", async (req,res)=>{
-  const users = await Model.find().lean();
-res.render("view", {users})
-})
+// app.get("/view", async (req,res)=>{
+// const allUsers = await Model.find().lean();
+// res.render("view", { users: allUsers });
+// })
 
 // app.get('/view', async (req, res) => {
 
